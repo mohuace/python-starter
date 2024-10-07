015e3cccb3970fc6cacfefc897650ab9e3b23e96
mohit98thaker <mohuace@gmail.com>
2024-10-07 17:25:18 -0400

    revisiting

--- oop_in_python/Employee.py
+++ oop_in_python/Employee.py
@@ -11,12 +11,22 @@
 # #This is a new property and will only be added to Steve object
 # Steve.title = "Manager"
 # print(Steve.title)
+#
+# Carol = Employee()
+#will throw error
+#print(Carol.title)
+
+#TODO: ASK if this is the case where we can add properties to the object
+#on the fly, what is the point of having properties defined in the class
+#when you don't know whether the objects will be having only those properties
+#or some extra ones as well
 
 
 
 class Employee:
     #Init is nothing but a constructor and self is similar to "this" in Java
     #We can also have default values --> that would make parameters optional
+    #This is method overloading
     def __init__(self, ID = -1, salary = 0, department = None):
         self.ID = ID
         self.salary = salary
